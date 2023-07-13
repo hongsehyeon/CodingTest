67f1e5a02461384a01db455d59924e7aef37df8e
홍세현 <80824477+hsh0517@users.noreply.github.com>
2023-07-13 19:57:17 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "", temp = "";
+    vector <int> v;
+    for (int i=0; i<s.size(); i++) {
+        if (s[i] == ' ') {
+            v.push_back(stoi(temp));
+            temp = "";
+        } else temp += s[i];
+    }
+    v.push_back(stoi(temp));
+    sort(v.begin(), v.end());
+    string min = to_string(v[0]);
+    string max = to_string(v[v.size()-1]);
+    return min + " " + max;
+}
