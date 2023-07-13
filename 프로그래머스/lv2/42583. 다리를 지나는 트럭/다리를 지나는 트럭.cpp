196b00f3a3af8194a23efb60687be31365928235
홍세현 <80824477+hsh0517@users.noreply.github.com>
2023-07-13 19:57:10 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    queue<pair<int, int>> bridge;
+    int time = 0, w = 0;
+    for (int i = 0; i < truck_weights.size(); i++) {
+        time++;
+        if (time == bridge.front().second + bridge_length) {
+            w -= bridge.front().first;
+            bridge.pop();
+        }
+        while (w + truck_weights[i] > weight) {
+            time = bridge.front().second + bridge_length;
+            w -= bridge.front().first;
+            bridge.pop();
+        }
+        w += truck_weights[i];
+        bridge.emplace(truck_weights[i], time);
+    }
+    time = bridge.back().second + bridge_length;
+    return time;
+}
